d46dad8e752e528812c5511db05776ca6da79173
tobi-mwnr-heyrise <tobias.mitterwallner@heyrise.com>
2024-10-25 14:28:25 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <h1
+    class="bg-white border border-gray-200 flex flex-col items-center justify-center px-4 md:px-8 lg:px-24 py-8 rounded-lg shadow-2xl"
+  >
+    Hello World
+  </h1>
+</template>
